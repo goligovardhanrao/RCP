1955a242b6a70b76e7b491d0b290ccdb9d5c7075
goligovardhanrao <goligovardhanrao527@gmail.com>
2022-03-09 22:40:18 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,19 +186,20 @@
       320: {
         slidesPerView: 1,
         spaceBetween: 20
-      },
-      575: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 20
       }
+      // ,
+      // 575: {
+      //   slidesPerView: 2,
+      //   spaceBetween: 20
+      // },
+      // 768: {
+      //   slidesPerView: 3,
+      //   spaceBetween: 20
+      // },
+      // 992: {
+      //   slidesPerView: 5,
+      //   spaceBetween: 20
+      // }
     }
   });
 
